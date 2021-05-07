e958f0935bc1ba8350567eccd000db39b55c3680
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-05-07 10:52:33 +0500

    changed article to heading

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -4,7 +4,7 @@ function Article(params) {
   console.log(params);
   return (
     <div className="max-w-sm rounded overflow-hidden mt-2 p-3 shadow-lg bg-red-50 ">
-      <h3 className="text-center">{params.title}</h3>
+      <h3 className="text-center font-bold">{params.title}</h3>
       <p>{params.body}</p>
     </div>
   );
